eeb2b8ec3e275b9c687a24e3c10239853deb7d46
Varun-Lall <vlall_be16@thapar.edu>
2018-02-08 16:13:17 +0530

    successfully committed

---
+++ test.py
@@ -0,0 +1,6 @@
+HomeTown={"Prashant":"Delhi", "Govind":"Gwalior", "Anil":"Morena", "Pankaj":"Agra"}
+print "Original Dictionary: ", HomeTown
+print "Home Town of Prashant is :", HomeTown["Prashant"]
+print "Home Town of Govind is :", HomeTown["Govind"]
+print "Home Town of Anil is :", HomeTown["Anil"]
+print "Home Town of Pankaj is :", HomeTown["Pankaj"]
